3777a4d7b5a1c6f6aac7b2da850489f341c1d9e2
otukutun <orehaorz@gmail.com>
2016-12-20 17:01:16 +0900

    groupedテーブルView作ってみた.

--- GroupTableView/ViewController.swift
+++ GroupTableView/ViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate  = self
     }
+    
+    let sectionArray: Array = ["Tokyo", "Oosaka"]
+    
+    let tokyoRows: Array = ["Shibuya", "Shinjuku", "Ikebukuro"]
+    let oosakaRows: Array = ["Umeda", "Oosaka"]
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var tableView: UITableView!
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return tokyoRows.count
+        } else if section == 1 {
+            return oosakaRows.count
+        } else {
+            return 0
+        }
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionArray[section] as String
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50.0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = "\(tokyoRows[indexPath.row])"
+        } else if indexPath.section == 1 {
+            cell.textLabel?.text = "\(oosakaRows[indexPath.row])"
+        }
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionArray.count
+    }
+
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        if index == 0 {
+            return tokyoRows.count
+        } else if index == 1 {
+            return oosakaRows.count
+        } else {
+            return 0
+        }
+    }
 }
 
